4c5fd35c997654d912748f749f0e608259a194a6
windevopscloud <windevopscloud@gmailcom>
2025-08-17 14:22:06 +0400

    first

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,7 @@
 # -----------------------------
 # VPC
 # -----------------------------
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
